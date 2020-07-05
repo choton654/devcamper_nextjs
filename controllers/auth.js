872a95fae75cc6718480ad9caf6a8302f642a5ed
Choton <choton654@gmail.com>
2020-07-05 22:45:04 +0530

    add logout route

--- controllers/auth.js
+++ controllers/auth.js
@@ -53,6 +53,18 @@ exports.login = asyncMiddleware(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc   logout user
+// route   GET /api/v1/auth/logout
+// access  private
+exports.logout = asyncMiddleware(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({ success: true, data: {} });
+});
+
 // @desc   get logged in user
 // route   GET /api/v1/auth/me
 // access  private
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const {
   register,
   login,
+  logout,
   getMe,
   forgotPassword,
   resetPassword,
@@ -12,6 +13,7 @@ const { protect } = require('../middleware/auth');
 
 router.route('/register').post(register);
 router.route('/login').post(login);
+router.route('/logout').get(logout);
 router.route('/me').get(protect, getMe);
 router.route('/forgotpassword').post(forgotPassword);
 router.route('/updatedetails').put(protect, updateDetails);
